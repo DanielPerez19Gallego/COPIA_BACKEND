831ac03ba841ec8f3856e1d71e72e8a3f842b667
Yirmel-Sanchez <yirmelaugusto.sanchez@alu.uclm.es>
2022-10-09 18:30:21 +0200

    commit eclipse 2

---
+++ JUnit5Prueba/src/Tienda.java
@@ -0,0 +1,18 @@
+
+public class Tienda {
+	private int productos;
+	
+	public Tienda(int productos) {
+		this.productos = productos;
+	}
+
+	public int getProductos() {
+		return productos;
+	}
+
+	public void setProductos(int productos) {
+		this.productos = productos;
+	}
+	
+	
+}
