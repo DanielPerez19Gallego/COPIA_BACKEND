d4c19fae07b5a7006552688125cac7247fb55900
ignaciomartinez12 <91479589+ignaciomartinez12@users.noreply.github.com>
2022-10-25 01:24:01 +0200

    Add files via upload

--- TIComo_project/src/main/java/Equipo3/TIComo_project/http/UserController.java
+++ TIComo_project/src/main/java/Equipo3/TIComo_project/http/UserController.java
@@ -16,13 +16,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import Equipo3.TIComo_project.model.User;
 import Equipo3.TIComo_project.services.UserService;
-import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
 
 
 @RestController
@@ -31,22 +29,19 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-
+	
 	private String correo = "correo";
-
-	private String rol = "rol";
-	@CrossOrigin
+	
 	@PostMapping("/login")
 	public ResponseEntity<String> login(HttpSession session, @RequestBody Map<String, Object> info) {
 		try {
 			JSONObject jso = new JSONObject(info);
 			String response = this.userService.login(jso);
-
+			
 			if (response.equals("nulo"))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuario o password desconocidas");
 			else {
 				session.setAttribute(this.correo, jso.getString(this.correo));
-				System.out.println("gñe");
 				return new ResponseEntity<>("Inicio de sesion correcto como "+ response, HttpStatus.OK);
 			}
 		} catch (Exception e) {
@@ -57,73 +52,69 @@ public class UserController {
 	public ResponseEntity<String> register(@RequestBody Map<String, Object> info, HttpSession session) {
 		try {
 			JSONObject jso = new JSONObject(info);
-			String pwd1 = jso.getString("pwd1");
-			String pwd2 = jso.getString("pwd2");
-
-			if (!pwd1.equals(pwd2))
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Las contraseñas tienen que ser iguales");
-			else {
-				if(pwd1.length() < 8)
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La contraseña tiene que tener minimo 8 caracteres");
-			}
-
-			String response = this.userService.register(jso);
-
+			String response = "";
+			String [] comprobar = this.userService.comprobarPassword(jso);
+			if (Boolean.TRUE.equals(Boolean.valueOf(comprobar[0])))
+				response = this.userService.register(jso);
+			else
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, comprobar[1]);
+			
 			if (response.equals(this.correo))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ya existe un usuario con ese correo");
 			else {
-				return new ResponseEntity<>("Registro completado", HttpStatus.OK);
+				return new ResponseEntity<>(response, HttpStatus.OK);
 			}
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-
+	
 	@PostMapping("/crearUsuario")
 	public ResponseEntity<String> crearUsuario(@RequestBody Map<String, Object> info, HttpSession session) {
 		try {
 			JSONObject jso = new JSONObject(info);
 			String pwd1 = jso.getString("pwd1");
 			String pwd2 = jso.getString("pwd2");
-
+			
 			if (!pwd1.equals(pwd2))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Las contraseñas tienen que ser iguales");
 			else {
 				if(pwd1.length() < 8)
 					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La contraseña tiene que tener minimo 8 caracteres");
 			}
-
+				
 			String response = this.userService.crearUsuario(jso);
-
+			
 			if (response.equals(this.correo))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ya existe un usuario con ese correo");
 			else {
-				return new ResponseEntity<>(jso.getString("rol")+" creado correctamente", HttpStatus.OK);
+				return new ResponseEntity<>(response, HttpStatus.OK);
 			}
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-
+	
 	@PostMapping("/eliminarUsuario")
 	public ResponseEntity<String> eliminarUsuario(@RequestBody Map<String, Object> info, HttpSession session) {
 		try {
 			JSONObject jso = new JSONObject(info);
 			String correoUsuario = jso.getString(this.correo);
 			String response = this.userService.eliminarUsuario(correoUsuario);
-
+			
 			if (response.equals(this.correo))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe un usuario con ese correo");
 			else {
-				return new ResponseEntity<>("Usuario eliminado correctamente", HttpStatus.OK);
+				return new ResponseEntity<>(response, HttpStatus.OK);
 			}
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
+	
 	@CrossOrigin
 	@GetMapping("/getUsuarios")//Devuelve todos los Users
-	public ResponseEntity<String> consultarUsuarios() {
+	public List<User> consultarUsuarios() {
 		List <User> listaResponse;
 		try {
 			listaResponse = this.userService.consultarUsuarios();  //Recojo la lista en una variable.
@@ -131,13 +122,14 @@ public class UserController {
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe ningun usuario en la base de datos");
 			}else {
 				JSONObject jso = new JSONObject(); //Mando la lista con todos los users en un JSON.
-				return new ResponseEntity<>(jso.put("todos",listaResponse)+"Todos los usuarios actuales de la base de datos.", HttpStatus.OK);
+				return listaResponse;
 			}
 
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
+	
 	@CrossOrigin
 	@GetMapping("/getUsuario/{correo}")//Devuelve un único user.
 	public ResponseEntity<String> consultarUsuario(@PathVariable String correo) {
@@ -161,6 +153,7 @@ public class UserController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
+	
 	@PutMapping("/actualizarUsuario/{correo}")
 	public ResponseEntity<String> actualizarUsuario(@PathVariable("correo") String correo,@RequestBody Map<String, Object> info){
 		JSONObject json = new JSONObject(info);
@@ -179,7 +172,5 @@ public class UserController {
 		}
 
 	}
+	
 }
-
-
-
