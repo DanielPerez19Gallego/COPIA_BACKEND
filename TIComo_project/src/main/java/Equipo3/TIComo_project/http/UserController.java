422210a2f72a51f1ad864afe7a7f10e8588d171d
ignaciomartinez12 <91479589+ignaciomartinez12@users.noreply.github.com>
2022-10-25 20:33:00 +0200

    Controladores con todas las funcionalidades

--- TIComo_project/src/main/java/Equipo3/TIComo_project/http/UserController.java
+++ TIComo_project/src/main/java/Equipo3/TIComo_project/http/UserController.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import Equipo3.TIComo_project.model.Admin;
+import Equipo3.TIComo_project.model.Client;
+import Equipo3.TIComo_project.model.Rider;
 import Equipo3.TIComo_project.model.User;
 import Equipo3.TIComo_project.services.UserService;
 
@@ -29,15 +31,15 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	private String correo = "correo";
-	
+
 	@PostMapping("/login")
 	public ResponseEntity<String> login(HttpSession session, @RequestBody Map<String, Object> info) {
 		try {
 			JSONObject jso = new JSONObject(info);
 			String response = this.userService.login(jso);
-			
+
 			if (response.equals("nulo"))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuario o password desconocidas");
 			else {
@@ -58,7 +60,7 @@ public class UserController {
 				response = this.userService.register(jso);
 			else
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, comprobar[1]);
-			
+
 			if (response.equals(this.correo))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ya existe un usuario con ese correo");
 			else {
@@ -68,23 +70,23 @@ public class UserController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-	
+
 	@PostMapping("/crearUsuario")
 	public ResponseEntity<String> crearUsuario(@RequestBody Map<String, Object> info, HttpSession session) {
 		try {
 			JSONObject jso = new JSONObject(info);
 			String pwd1 = jso.getString("pwd1");
 			String pwd2 = jso.getString("pwd2");
-			
+
 			if (!pwd1.equals(pwd2))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Las contraseñas tienen que ser iguales");
 			else {
 				if(pwd1.length() < 8)
 					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "La contraseña tiene que tener minimo 8 caracteres");
 			}
-				
+
 			String response = this.userService.crearUsuario(jso);
-			
+
 			if (response.equals(this.correo))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Ya existe un usuario con ese correo");
 			else {
@@ -94,14 +96,14 @@ public class UserController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-	
+
 	@PostMapping("/eliminarUsuario")
 	public ResponseEntity<String> eliminarUsuario(@RequestBody Map<String, Object> info, HttpSession session) {
 		try {
 			JSONObject jso = new JSONObject(info);
 			String correoUsuario = jso.getString(this.correo);
 			String response = this.userService.eliminarUsuario(correoUsuario);
-			
+
 			if (response.equals(this.correo))
 				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe un usuario con ese correo");
 			else {
@@ -111,49 +113,7 @@ public class UserController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-	
-	@CrossOrigin
-	@GetMapping("/getUsuarios")//Devuelve todos los Users
-	public List<User> consultarUsuarios() {
-		List <User> listaResponse;
-		try {
-			listaResponse = this.userService.consultarUsuarios();  //Recojo la lista en una variable.
-			if(listaResponse.isEmpty()) {
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe ningun usuario en la base de datos");
-			}else {
-				JSONObject jso = new JSONObject(); //Mando la lista con todos los users en un JSON.
-				return listaResponse;
-			}
 
-		} catch (Exception e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-		}
-	}
-	
-	@CrossOrigin
-	@GetMapping("/getUsuario/{correo}")//Devuelve un único user.
-	public ResponseEntity<String> consultarUsuario(@PathVariable String correo) {
-		List <User> unico;
-
-		try {
-			unico = this.userService.consultarUsuario(correo);  //El usuario que quiero lo meto en la variable.
-			if(!unico.isEmpty()) {
-				JSONObject jso = new JSONObject(); 
-				for(int i = 0;i<unico.size();i++) {
-					if(unico.get(i).getCorreo().equals(correo)) {
-						jso.put("lista",unico);
-					}
-				}
-				return new ResponseEntity<>(jso+"usuario encontrado", HttpStatus.OK); //Mando el user en json
-			}else {
-				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe ese usuario en la base de datos");
-			}
-
-		} catch (Exception e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-		}
-	}
-	
 	@PutMapping("/actualizarUsuario/{correo}")
 	public ResponseEntity<String> actualizarUsuario(@PathVariable("correo") String correo,@RequestBody Map<String, Object> info){
 		JSONObject json = new JSONObject(info);
@@ -170,7 +130,51 @@ public class UserController {
 		}else {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe ese usuario en la base de datos");
 		}
-
 	}
-	
+
+	@GetMapping("/getRiders")
+	public ResponseEntity<String> consultarRiders() {
+		List<Rider> listaResponse;
+		try {
+			listaResponse = this.userService.consultarRiders();  //Recojo la lista en una variable.
+			if(listaResponse.isEmpty()) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe ningun usuario en la base de datos");
+			}else {
+				return new ResponseEntity<>(this.userService.userRiders(listaResponse), HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+	}
+
+	@GetMapping("/getAdmins")
+	public ResponseEntity<String> consultarAdmins() {
+		List<Admin> listaResponse;
+		try {
+			listaResponse = this.userService.consultarAdmins();  //Recojo la lista en una variable.
+			if(listaResponse.isEmpty()) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe ningun usuario en la base de datos");
+			}else {
+				return new ResponseEntity<>(this.userService.userAdmins(listaResponse), HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+	}
+
+	@GetMapping("/getClients")
+	public ResponseEntity<String> consultarClients() {
+		List<Client> listaResponse;
+		try {
+			listaResponse = this.userService.consultarClients();  //Recojo la lista en una variable.
+			if(listaResponse.isEmpty()) {
+				throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No existe ningun usuario en la base de datos");
+			}else {
+				return new ResponseEntity<>(this.userService.userClients(listaResponse), HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+		}
+	}
 }
