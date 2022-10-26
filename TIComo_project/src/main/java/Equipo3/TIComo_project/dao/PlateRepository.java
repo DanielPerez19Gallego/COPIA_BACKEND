e5c2916e18f7ea38c70614832449d5f082f12ed9
ignaciomartinez12 <91479589+ignaciomartinez12@users.noreply.github.com>
2022-10-27 00:13:21 +0200

    Add files via upload

--- TIComo_project/src/main/java/Equipo3/TIComo_project/dao/PlateRepository.java
+++ TIComo_project/src/main/java/Equipo3/TIComo_project/dao/PlateRepository.java
@@ -3,14 +3,23 @@ package Equipo3.TIComo_project.dao;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import Equipo3.TIComo_project.model.Plate;
 
 public interface PlateRepository extends MongoRepository <Plate, String> {
-	Plate findByNombre(String nombre);
+	
+	@Query("select nombre from Plates p where nombre=?1 and nombreRestaurante=?2")
+	Plate findPlato(String nombre, String nombreRes);
+	
+	@Query("select nombre, nombreRestaurante from Plates p where nombre=?1 and nombreRestaurante=?2")
+	List<Plate> findByNombreAndRestaurante(String nombre, String nombreRes);
 
-	void deleteByNombre(String nombrePlato);
+	@Query("delete from Plates p where nombre=?1 and nombreRestaurante=?2")
+	void deletePlato(String nombrePlato, String nombreRestaurante);
 	
 	List <Plate> findBynombreRestaurante(String nombreRes);
+
+	void deleteBynombreRestaurante(String nombreRes);
 
 }
