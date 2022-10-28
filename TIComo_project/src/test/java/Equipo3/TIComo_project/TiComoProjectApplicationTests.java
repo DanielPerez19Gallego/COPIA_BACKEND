136d3ebf458b986a7f9150600106fc9ee3aa9ca9
ignaciomartinez12 <91479589+ignaciomartinez12@users.noreply.github.com>
2022-10-28 19:56:50 +0200

    Add files via upload

--- TIComo_project/src/test/java/Equipo3/TIComo_project/TiComoProjectApplicationTests.java
+++ TIComo_project/src/test/java/Equipo3/TIComo_project/TiComoProjectApplicationTests.java
@@ -1,6 +1,7 @@
 package Equipo3.TIComo_project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,53 +9,100 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import Equipo3.TIComo_project.services.UserService;
+import Equipo3.TIComo_project.services.FoodService;
 
 @SpringBootTest
 class TiComoProjectApplicationTests {
 
+	
 	@Autowired
-	private UserService user;
+	private FoodService food;
 	
 	@Test
-	void loginTest() throws JSONException {
+	void testFoodService() throws JSONException {
+		assertEquals("", food.consultarRestaurantes());
+		this.crearRestauranteTest();
+		assertNotSame("", food.consultarRestaurantes());
+		assertEquals("", food.platosParaEnviar("prueba"));
+		this.crearPlatoTest();
+		assertNotSame("", food.platosParaEnviar("prueba"));
+		this.actualizarPlatoTest();
+		this.actualizarRestauranteTest();
+		this.eliminarCartaTest();
+		this.eliminarRestauranteTest();
+	}
+
+	void crearRestauranteTest() throws JSONException {
 		JSONObject jso = new JSONObject();
-		jso.put("pwd", "1234567890");
-		jso.put("correo", "dani@gmail");
-		JSONObject jsoo = new JSONObject();
-		jsoo.put("pwd", "1234567890");
-		jsoo.put("correo", "dani@");
-		assertEquals("rider", user.login(jso));
-		assertEquals("nulo", user.login(jsoo));
+		jso.put("nombre", "comoti");
+		jso.put("categoria", "hola");
+		jso.put("cif", "cif");
+		jso.put("direccion", "ticomo");
+		jso.put("email", "hola");
+		jso.put("razonSocial", "ticomo");
+		jso.put("telefono", "hola");
+		assertEquals("Restaurante creado correctamente", food.crearRestaurante(jso));
+		jso.put("nombre","prueba");
+		assertEquals("Restaurante creado correctamente", food.crearRestaurante(jso));
+		assertEquals("nombre", food.crearRestaurante(jso));
+		
+	}
+
+	void crearPlatoTest() throws JSONException {
+		JSONObject jso = new JSONObject();
+		jso.put("nombre", "platoPrueba1");
+		jso.put("aptoVegano", "true");
+		jso.put("descripcion", "cif");
+		jso.put("precio", "ticomo");
+		jso.put("nombreRestaurante", "prueba");
+		jso.put("foto", "ticomo");
+		assertEquals("Plato creado correctamente", food.crearPlato(jso));
+		assertEquals("nombre", food.crearPlato(jso));
+		jso.put("nombre", "platoPrueba2");
+		assertEquals("Plato creado correctamente", food.crearPlato(jso));
+	}
+
+	void actualizarPlatoTest() throws JSONException {
+		JSONObject jso = new JSONObject();
+		jso.put("nombreViejo", "noexiste");
+		jso.put("aptoVegano", "true");
+		jso.put("descripcion", "cif");
+		jso.put("precio", "ticomo");
+		jso.put("nombreRestaurante", "prueba");
+		jso.put("foto", "ticomo");
+		jso.put("nombre", "Pizza");
+		assertEquals("noexiste", food.actualizarPlato(jso));
+		jso.put("nombreViejo", "platoPrueba2");
+		jso.put("nombre", "platoPrueba1");
+		assertEquals("nombre", food.actualizarPlato(jso));
+		jso.put("nombre", "Pizza");
+		assertEquals("Plato actualizado correctamente", food.actualizarPlato(jso));
+		
 	}
 	
-	@Test
-	void comprobarPassword() throws JSONException {
-		JSONObject iguales = new JSONObject();
-		iguales.put("pwd1", "12");
-		iguales.put("pwd2", "21");
-		JSONObject size = new JSONObject();
-		size.put("pwd1", "12");
-		size.put("pwd2", "12");
-		JSONObject digit = new JSONObject();
-		digit.put("pwd1", "Holaholaa");
-		digit.put("pwd2", "Holaholaa");
-		JSONObject minus = new JSONObject();
-		minus.put("pwd1", "hola4321");
-		minus.put("pwd2", "hola4321");
-		JSONObject mayus = new JSONObject();
-		mayus.put("pwd1", "HOLA1234");
-		mayus.put("pwd2", "HOLA1234");
-		JSONObject perfe = new JSONObject();
-		perfe.put("pwd1", "Hola1234");
-		perfe.put("pwd2", "Hola1234");
-		assertEquals("false", user.comprobarPassword(iguales)[0]);
-		assertEquals("false", user.comprobarPassword(size)[0]);
-		assertEquals("false", user.comprobarPassword(minus)[0]);
-		assertEquals("false", user.comprobarPassword(mayus)[0]);
-		assertEquals("true", user.comprobarPassword(perfe)[0]);
-		assertEquals("false", user.comprobarPassword(digit)[0]);
-		
+	void actualizarRestauranteTest() throws JSONException {
+		JSONObject jso = new JSONObject();
+		jso.put("nombre", "noexiste");
+		jso.put("categoria", "hola");
+		jso.put("cif", "cif");
+		jso.put("direccion", "ticomo");
+		jso.put("email", "hola");
+		jso.put("razonSocial", "ticomo");
+		jso.put("telefono", "hola");
+		assertEquals("nombre", food.actualizarRestaurante(jso));
+		jso.put("nombre", "comoti");
+		assertEquals("Restaurante actualizado correctamente", food.actualizarRestaurante(jso));
+	}
+	
+	void eliminarCartaTest() throws JSONException {
+		assertEquals("Carta eliminada correctamente", food.eliminarCarta("prueba"));
+		assertEquals("nombre", food.eliminarCarta("noexiste"));
+	}
+	
+	void eliminarRestauranteTest() throws JSONException {
+		assertEquals("Restaurante eliminado correctamente", food.eliminarRestaurante("prueba"));
+		assertEquals("Restaurante eliminado correctamente", food.eliminarRestaurante("comoti"));
+		assertEquals("nombre", food.eliminarRestaurante("noexiste"));
 	}
 
 }
