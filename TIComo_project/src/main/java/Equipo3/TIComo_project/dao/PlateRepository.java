59a3566d5f827817468ce560c66d86293b8cbafa
ignaciomartinez12 <91479589+ignaciomartinez12@users.noreply.github.com>
2022-10-31 12:55:48 +0100

    Add files via upload

--- TIComo_project/src/main/java/Equipo3/TIComo_project/dao/PlateRepository.java
+++ TIComo_project/src/main/java/Equipo3/TIComo_project/dao/PlateRepository.java
@@ -3,8 +3,11 @@ package Equipo3.TIComo_project.dao;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import Equipo3.TIComo_project.model.Plate;
 
+@Repository
 public interface PlateRepository extends MongoRepository <Plate, String> {
 
 	@Query(value = "{ 'nombre' : ?0, 'nombreRestaurante' : ?1 }")
