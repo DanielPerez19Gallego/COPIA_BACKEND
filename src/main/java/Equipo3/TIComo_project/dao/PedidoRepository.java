5275ce6ea80396453afe87b923792761062b75a1
ignaciomartinez12 <91479589+ignaciomartinez12@users.noreply.github.com>
2022-11-17 12:07:51 +0100

    Add files via upload

--- src/main/java/Equipo3/TIComo_project/dao/PedidoRepository.java
+++ src/main/java/Equipo3/TIComo_project/dao/PedidoRepository.java
@@ -3,6 +3,7 @@ package Equipo3.TIComo_project.dao;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import Equipo3.TIComo_project.model.Pedido;
 
@@ -14,4 +15,7 @@ public interface PedidoRepository extends MongoRepository <Pedido, String> {
 	List<Pedido> findAllByRestaurante(String restaurante);
 	Pedido findByidPedido(String idPedido);
 	void deleteByidPedido(String idPedido);
+	
+	@Query(value = "{ 'restaurante' : ?0, 'estado' : ?1 }")
+	List <Pedido> findAllByRestauranteAndEstado(String restaurante, int estado);
 }
